a4f44ce3b608c6c45158936cc1905f64a69ea31a
Ishan <ishanwijes@gmail.com>
2022-12-13 06:20:37 +1100

    changed typeDefs

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -3,7 +3,6 @@ const { gql } = require('apollo-server');
 
 
 module.exports = gql`
-
   type Post {
     id: ID!
     body: String!
@@ -14,20 +13,17 @@ module.exports = gql`
     likeCount: Int!
     commentCount: Int!
   }
-
   type Comment {
     id: ID!
     createdAt: String!
     username: String!
     body: String!
   }
-
   type Like {
     id: ID!
     createdAt: String!
     username: String!
   }
-
   type User {
     id: ID!
     email: String!
@@ -35,19 +31,16 @@ module.exports = gql`
     username: String!
     createdAt: String!
   }
-
   input RegisterInput {
     username: String!
     password: String!
     confirmPassword: String!
     email: String!
   }
-
   type Query {
     getPosts: [Post]
     getPost(postId: ID!): Post
   }
-
   type Mutation {
     register(registerInput: RegisterInput): User!
     login(username: String!, password: String!): User!
@@ -57,5 +50,4 @@ module.exports = gql`
     deleteComment(postId: ID!, commentId: ID!): Post!
     likePost(postId: ID!): Post!
   }
-  
 `;

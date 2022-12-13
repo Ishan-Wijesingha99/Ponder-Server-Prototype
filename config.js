425c97a2f69ccd7dfa87a6a3cc34f62f298be1ac
Ishan <ishanwijes@gmail.com>
2022-12-14 00:03:18 +1100

    changed config file

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   MONGODB: 'mongodb+srv://ishanwij99:yeet981@cluster0.6n9gpsy.mongodb.net/Ponder?retryWrites=true&w=majority',
-  SECRET_KEY: 'some very secret key'
-};
+  SECRET_KEY: 'fjkdsnkjng09450235608nmgdfmvlso'
+}

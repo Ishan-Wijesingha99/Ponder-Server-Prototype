8005c9abd42d4bfd5ac920a50d2b10f99584cfd8
Ishan <ishanwijes@gmail.com>
2022-12-14 00:05:09 +1100

    update index.js file

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const { ApolloServer, PubSub } = require('apollo-server');
-const mongoose = require('mongoose');
+const { ApolloServer, PubSub } = require('apollo-server')
 
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config.js');
+const mongoose = require('mongoose')
 
-const pubsub = new PubSub();
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 
-const PORT = process.env.PORT || 5000;
+const { MONGODB } = require('./config.js')
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ req, pubsub })
-});
+const pubsub = new PubSub()
 
-mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
-  .then(() => {
-    console.log('MongoDB Connected');
-    return server.listen({ port: PORT });
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
-  })
-  .catch(err => {
-    console.error(err)
-  })
+const PORT = process.env.PORT || 5000
+
+
+
+const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => ({ req, pubsub }) })
+
+
+
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+.then(() => {
+  console.log('MongoDB Connected')
+  return server.listen({ port: PORT })
+})
+.then(res => {
+  console.log(`Server running at ${res.url}`)
+})
+.catch(err => {
+  console.error(err)
+})
